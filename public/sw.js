6bf4e13f165f8f494e1bc4927d19699dbe3b6cf4
xiaohong <55377146@qq.com>
2026-05-07 16:39:22 +0800

    aa

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // 直接放行所有网络请求，绝不缓存，确保不会影响任何原有的实时数据功能
+  event.respondWith(fetch(event.request));
+});
