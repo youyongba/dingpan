4e80bbe6ea74055cd4089260721bc56f2f57bc48
xiaohong <55377146@qq.com>
2026-06-02 00:24:28 +0800

    aaa

--- public/sw.js
+++ public/sw.js
@@ -9,4 +9,4 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   // 直接放行所有网络请求，绝不缓存，确保不会影响任何原有的实时数据功能
   event.respondWith(fetch(event.request));
-});
+});// version 2 - cache busting
