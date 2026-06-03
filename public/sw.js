e07c87dc22a61b4d89bfdf4cd7730970ba7a9725
xiaohong <55377146@qq.com>
2026-06-03 09:14:29 +0800

    质量巡查参数工程ID参数写错了

--- public/sw.js
+++ public/sw.js
@@ -10,3 +10,4 @@ self.addEventListener('fetch', event => {
   // 直接放行所有网络请求，绝不缓存，确保不会影响任何原有的实时数据功能
   event.respondWith(fetch(event.request));
 });// version 2 - cache busting
+// version 3 - cache busting
